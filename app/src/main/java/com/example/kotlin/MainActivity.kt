3d365ebe28d20c07bddcf0928e563672a6e6c108
minoo <minwoo@dailyshot.co>
2022-07-06 19:47:18 +0900

    Test: Lint

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val value = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if(value == 0) {
+
+            findViewById<TextView>(R.id.tv_content)
+                .text = "ssss"
+        } else {
+
+
+            findViewById<TextView>(R.id.tv_content)
+                .text = "zzzz"
+        }
     }
 }
